697138ff007ff85452a12425cfb95586b49ef43f
devSouvik <souvikguria98@gmail.com>
2021-10-25 20:24:04 +0530

    updated all

---
+++ script.js
@@ -0,0 +1,14 @@
+function darkTheme(x) {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+
+  var y = document.getElementById("dark-name");
+  if (y.innerHTML === "Light Theme") {
+    y.innerHTML = "Dark Theme";
+  } else {
+    y.innerHTML = "Light Theme";
+  }
+  //   x.classList.toggle("location-light");
+}
+//   x.classList.toggle("dark-theme");
+//   x.classList.toggle("location-light");
